485ba411fbaead046ba2438a11bff7d13596e5a3
Alumno <Alumno@eq32-lab1>
2014-04-09 19:58:23 -0300

    primero proyecto

--- trabajo/src/trabajo/trabajo.java
+++ trabajo/src/trabajo/trabajo.java
@@ -1,5 +1,22 @@
 package trabajo;
 
-public class trabajo {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
 
+public class Ventana {
+public static void main(String[] args) {
+
+		JFrame frame = new JFrame();
+		JLabel label = new JLabel();
+		label.setText("Hola soy una Label");
+		frame.add(label);
+		frame.setTitle("Mi segunda ventana");
+		frame.setSize(800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+
+
+	}
 }
